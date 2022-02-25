fd518b35b8bdd70cbe38d1b7b9ba9c7cdde288d1
Ohta Akira <waterlow3442@gmail.com>
2022-02-25 21:53:31 +0900

    ecs用のiamロール

--- iam/aws_iam_role.tf
+++ iam/aws_iam_role.tf
@@ -2,3 +2,8 @@ resource "aws_iam_role" "ecs_instance_role" {
   name               = "rails_kata_ecs_instance_role"
   assume_role_policy = file("aws_iam_role_policies/ec2_assume_role_policy.json")
 }
+
+resource "aws_iam_role" "ecs_task_role" {
+  name               = "rails-kata-ecs-task-role"
+  assume_role_policy = file("aws_iam_role_policies/ecs_task_assume_role_policy.json")
+}
