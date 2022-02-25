c86e6cd6acbc254f6944e244e63e66c407e09674
Ohta Akira <waterlow3442@gmail.com>
2022-02-26 00:09:39 +0900

    lbの設定を追加

--- service/aws_ecs_service.tf
+++ service/aws_ecs_service.tf
@@ -4,4 +4,10 @@ resource "aws_ecs_service" "rails-kata-web-service" {
   task_definition = aws_ecs_task_definition.rails-kata-web-task.arn
   desired_count   = 1
   launch_type     = "EC2"
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.http.arn
+    container_name   = "rails-kata-web"
+    container_port   = "3000"
+  }
 }
--- service/aws_security_group.tf
+++ service/aws_security_group.tf
@@ -2,6 +2,35 @@ resource "aws_security_group" "instance" {
   name   = "rails-kata-instance"
   vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
 
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+
+    security_groups = [
+      aws_security_group.alb.id
+    ]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name        = "rails-kata-alb"
+  description = "http and https"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
