e0e4c821f309441ca9b5f856615819ab2736accf
Ohta Akira <waterlow3442@gmail.com>
2022-03-01 10:21:22 +0900

    delayed_jobの設定

--- service/aws_ecs_task_definition.tf
+++ service/aws_ecs_task_definition.tf
@@ -4,3 +4,17 @@ resource "aws_ecs_task_definition" "rails-kata-web-task" {
   task_role_arn         = data.terraform_remote_state.aws_iam.outputs.ecs_task_role_arn
   network_mode          = "bridge"
 }
+
+resource "aws_ecs_task_definition" "rails-kata-migration-task" {
+  family                = "rails-kata-migration"
+  container_definitions = file("./container_definitions/migration.json")
+  task_role_arn         = data.terraform_remote_state.aws_iam.outputs.ecs_task_role_arn
+  network_mode          = "bridge"
+}
+
+resource "aws_ecs_task_definition" "rails-kata-delayed-job-task" {
+  family                = "rails-kata-delayed-job"
+  container_definitions = file("./container_definitions/delayed_job.json")
+  task_role_arn         = data.terraform_remote_state.aws_iam.outputs.ecs_task_role_arn
+  network_mode          = "bridge"
+}
